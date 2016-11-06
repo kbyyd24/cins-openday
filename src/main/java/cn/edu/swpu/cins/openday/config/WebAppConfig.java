660d52c059d513182c429406289dfc074a8482bd
gaoyx <melo@gaoyuexiang.cn>
2016-11-06 20:27:39 +0800

    add cors mapping

--- src/main/java/cn/edu/swpu/cins/openday/config/WebAppConfig.java
+++ src/main/java/cn/edu/swpu/cins/openday/config/WebAppConfig.java
@@ -4,6 +4,7 @@ import cn.edu.swpu.cins.openday.config.interceptor.UserSecurityInterceptor;
 import cn.edu.swpu.cins.openday.service.CacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,6 +18,11 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 	@Autowired
 	public WebAppConfig(CacheService cacheService) {
 		this.cacheService = cacheService;
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/*").allowedOrigins("*");
 	}
 
 	@Override
