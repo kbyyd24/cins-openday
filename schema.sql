e61c2d2af764485ade28ea0faf1602aa2c626acc
gaoyx <melo@gaoyuexiang.cn>
2016-11-10 19:00:29 +0800

    update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `signUpUser` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `username` varchar(255) NOT NULL COMMENT 'username',
   `password` char(60) NOT NULL COMMENT 'password',
@@ -9,16 +9,15 @@ CREATE TABLE `signUpUser` (
   UNIQUE INDEX `user_mail_idx` (`mail` ASC) USING BTREE
 )DEFAULT CHAR SET utf8 AUTO_INCREMENT 1000;
 
-CREATE TABLE `group` (
+CREATE TABLE IF NOT EXISTS `group` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `group_name` varchar(255) NOT NULL COMMENT 'group name',
   `match_id` int NOT NULL COMMENT 'match id',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `group_group_idx` (`group_name` ASC) USING BTREE,
-  UNIQUE INDEX `group_match_idx` (`match_id` ASC) USING BTREE
+  UNIQUE INDEX `group_group_and_match_idx` (`group_name`, `match_id` ASC) USING BTREE
 )DEFAULT CHAR SET utf8 AUTO_INCREMENT 1000;
 
-CREATE TABLE `activity` (
+CREATE TABLE IF NOT EXISTS `activity` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'primary key',
   `title` varchar(255) NOT NULL COMMENT 'activity title',
   `content` varchar(255) NOT NULL COMMENT 'activity content',
@@ -29,17 +28,19 @@ CREATE TABLE `activity` (
   INDEX `activity_end_time_idx` (`end_time` ASC) USING BTREE
 )DEFAULT CHAR SET utf8 AUTO_INCREMENT 1000;
 
-CREATE TABLE `match` (
+CREATE TABLE IF NOT EXISTS `match` (
   `id` int(11) NOT NULL COMMENT 'primary key',
   `match_name` varchar(255) NOT NULL COMMENT 'name of match',
   `detail` varchar(255) NOT NULL COMMENT 'content of match',
-  start_time bigint NOT NULL COMMENT 'begin time',
+  `start_time` bigint NOT NULL COMMENT 'begin time',
   `end_time` bigint NOT NULL COMMENT 'end time',
+  `data_link` VARCHAR(255) DEFAULT NULL COMMENT 'data set file link',
+  `data_password` VARCHAR(4) DEFAULT NULL COMMENT 'password for get data set link',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `match_end_time_idx` (`end_time` ASC) USING BTREE
 )DEFAULT CHAR SET utf8 AUTO_INCREMENT 1000;
 
-CREATE TABLE registration (
+CREATE TABLE IF NOT EXISTS registration (
   `id` int(11) NOT NULL COMMENT 'primary key',
   `match_id` int(11) NOT NULL COMMENT 'match id',
   `user_id` int(11) NOT NULL COMMENT 'signUpUser id',
@@ -51,7 +52,7 @@ CREATE TABLE registration (
   UNIQUE INDEX `mugm_uniq` (`match_id` ASC, `user_id` ASC) USING BTREE
 )DEFAULT CHAR SET utf8 AUTO_INCREMENT 1000;
 
-CREATE TABLE `score` (
+CREATE TABLE IF NOT EXISTS `score` (
   `id` int(11) NOT NULL COMMENT 'primary key',
   `regist_id` int(11) NOT NULL COMMENT 'registration id',
   `score` int NOT NULL COMMENT 'signUpUser score',
@@ -61,7 +62,7 @@ CREATE TABLE `score` (
 
 ALTER TABLE `group` ADD CONSTRAINT `group_match_ref` FOREIGN KEY (`match_id`) REFERENCES `match` (`id`);
 ALTER TABLE registration ADD CONSTRAINT `mugm_match_ref` FOREIGN KEY (`match_id`) REFERENCES `match` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE registration ADD CONSTRAINT `mugm_user_ref` FOREIGN KEY (`user_id`) REFERENCES `signUpUser` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE registration ADD CONSTRAINT `mugm_user_ref` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE registration ADD CONSTRAINT `mugm_group_ref` FOREIGN KEY (`group_id`) REFERENCES `group` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE `score` ADD CONSTRAINT `score_enlist_ref` FOREIGN KEY (regist_id) REFERENCES registration (`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
