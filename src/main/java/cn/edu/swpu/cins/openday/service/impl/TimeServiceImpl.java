033cb2f6954d65396011ec9ff67b3ced1354a5d2
gaoyx <melo@gaoyuexiang.cn>
2016-12-01 10:32:49 +0800

    set expire time

--- src/main/java/cn/edu/swpu/cins/openday/service/impl/TimeServiceImpl.java
+++ src/main/java/cn/edu/swpu/cins/openday/service/impl/TimeServiceImpl.java
@@ -25,7 +25,7 @@ public class TimeServiceImpl implements TimeService {
 	@Override
 	public long getMatchExpireTime() {
 		Calendar expireTime = Calendar.getInstance(Locale.CHINA);
-		expireTime.set(2016, Calendar.DECEMBER, 2, 0, 0, 0);
+		expireTime.set(2016, Calendar.DECEMBER, 1, 21, 0, 0);
 		return expireTime.getTimeInMillis();
 	}
 }
